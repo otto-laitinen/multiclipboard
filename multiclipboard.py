d021c240a5b8a3d677c2731ec8a8a9bb87b90cfc
Otto <otto.laitinen@edu.turkuamk.fi>
2022-01-27 12:12:02 +0200

    added save and load functions

--- multiclipboard.py
+++ multiclipboard.py
@@ -2,8 +2,24 @@ import sys
 import clipboard
 import json
 
-# argv is a variable that contains the arguments passed to a program through the command line.
 
+def save_items(filepath, data):
+    # Opens a file (where filepath is the location)
+    # in write mode (If file exists: overwrites it. If file doesn't exist: creates one)
+    # and stores it in variable f
+    with open(filepath, "w") as f:
+        json.dump(data, f)  # Saves data in var f in the JSON file
+
+
+def load_json(filepath):
+    with open(filepath, "r") as f:
+        data = json.load(f)
+        return data
+
+
+# argv is a variable that contains the arguments passed to a program through the command line.
+# The length needs to be 2 since the first item in argv is the name of the python file,
+# and the second one is the actual command.
 if len(sys.argv) == 2:
     command = sys.argv[1]
 
