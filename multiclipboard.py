188ff78ce78e89edb1d96859a2a1b487029e923b
Otto <otto.laitinen@edu.turkuamk.fi>
2022-01-27 11:57:37 +0200

    added allowed command types

--- multiclipboard.py
+++ multiclipboard.py
@@ -1,3 +1,19 @@
 import sys
 import clipboard
 import json
+
+# argv is a variable that contains the arguments passed to a program through the command line.
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+
+    if command == "save":
+        print("save")
+    elif command == "load":
+        print("load")
+    elif command == "list":
+        print("list")
+    else:
+        print("Unknown command")
+else:
+    print("Please pass only one command.")
